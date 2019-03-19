9391c151ea9d238b16247967e9b88e8289403ffa
stijnbrunia <48210655+stijnbrunia@users.noreply.github.com>
2019-03-19 14:50:35 +0100

    Validation data

--- Validation.py
+++ Validation.py
@@ -2,45 +2,7 @@ import flightdata_reader
 import xlrd 
 import numpy as np
 import matplotlib.pyplot as plt
-
-#Importing flight data ________________________________________________________
-flightdata = flightdata_reader.flightdata
-
-#Importing excel data _________________________________________________________
-file_location = 'Post_Flight_Datasheet_07_03_V3.xlsx'
-workbook = xlrd.open_workbook(file_location)
-sheet = workbook.sheet_by_index(0)
-
-#Definition that changes the time from the format 1:01:23 into seconds
-def timechange(t):
-    
-    tc = t*24*3600
-#    if len(t) == 4:
-#        tc = int(t[0:1])*60 + int(t[2:4])
-#    if len(t) == 5:
-#        tc = int(t[0:2])*60 + int(t[3:5])
-#    if len(t) == 7:
-#        tc = int(t[0])*3600 + int(t[2:4])*60 + int(t[5:7])
-    return tc
-    
-#Starting times of eigenmotions
-st_ph  = int(timechange(sheet.cell_value(82,3))) #starting time phugoid
-st_shp = int(timechange(sheet.cell_value(83,3))) #starting time short period
-st_dr  = int(timechange(sheet.cell_value(82,6))) #starting time dutch roll
-st_drd = int(timechange(sheet.cell_value(83,6))) #starting time dutch roll damp
-st_ar  = int(timechange(sheet.cell_value(82,9))) #starting time aperiodic roll 
-st_spi = int(timechange(sheet.cell_value(83,9))) #starting time spiral
-
-#Importing variables 
-time = flightdata['flightdata']['time']['data']
-AoA = flightdata['flightdata']['vane_AOA']['data']
-pitchA = flightdata['flightdata']['Ahrs1_Pitch']['data']
-pitchrate = flightdata['flightdata']['Ahrs1_bPitchRate']['data']
-yawrate = flightdata['flightdata']['Ahrs1_bYawRate']['data']
-rollrate = flightdata['flightdata']['Ahrs1_bRollRate']['data']
-rollA = flightdata['flightdata']['Ahrs1_Roll']['data']
-Vtrue = flightdata['flightdata']['Dadc1_tas']['data']
-truehead = flightdata['flightdata']['Fms1_trueHeading']['data']
+from Flight_data import *
 
 #______________________________________________________________________________
 #_________________________Creating lines to be plotted_________________________
@@ -215,19 +177,19 @@ rollA_spi = rollA[t_int_spi_st:t_int_spi_end]
 #------------------------------Plots Aperiodic role----------------------------
 #------------------------------------------------------------------------------
 
-#plt.plot(t_ar,AoA_ar,label="AoA")
-#plt.plot(t_ar,pitch_ar,label="Pitch angle")
-#plt.plot(t_ar,pitchrate_ar,label='Pitch Rate')
-#plt.plot(t_ar,rollrate_ar,label='Roll Rate')
-#plt.plot(t_ar,rollA_ar,label='Roll angle')
-#
-#plt.axvline(x=(st_ar+correction_ar),color='r')
-#plt.axhline(y=0,color='g')
-#
-#plt.title("Aperiodic Role")
-#plt.legend()
-#
-#plt.show
+plt.plot(t_ar,AoA_ar,label="AoA")
+plt.plot(t_ar,pitch_ar,label="Pitch angle")
+plt.plot(t_ar,pitchrate_ar,label='Pitch Rate')
+plt.plot(t_ar,rollrate_ar,label='Roll Rate')
+plt.plot(t_ar,rollA_ar,label='Roll angle')
+
+plt.axvline(x=(st_ar+correction_ar),color='r')
+plt.axhline(y=0,color='g')
+
+plt.title("Aperiodic Role")
+plt.legend()
+
+plt.show
 
 
 #------------------------------------------------------------------------------
